234b97cdef71e673ecf60deca0563b0a98fbd556
niekcandaele <niek@candaele.dev>
2021-03-09 22:13:29 +0100

    feat: Add image to gatsby app

--- packages/gatsby/src/pages/index.js
+++ packages/gatsby/src/pages/index.js
@@ -1,4 +1,5 @@
 import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 
 // styles
@@ -11,9 +12,13 @@ const pageStyles = {
 // markup
 const IndexPage = ({ data }) => {
   console.log(data);
+  console.log(data.strapiCampaign.mainImage);
+  const image = getImage(data.strapiCampaign.mainImage)
+  console.log(image);
   return (
     <main style={pageStyles}>
-      <p>Hey! This is a Gatsby site for the "{data.allStrapiCampaign.nodes[0].Name}" campaign</p>
+      <p>Hey! This is a Gatsby site for the "{data.strapiCampaign.Name}" campaign</p>
+      <GatsbyImage image={image} alt={data.strapiCampaign.Name} />
     </main>
   )
 }
@@ -23,13 +28,14 @@ export default IndexPage
 
 export const query = graphql`
 query MyQuery {
-  allStrapiCampaign {
-    nodes {
-      Name
-      mainImage {
-        url
+  strapiCampaign {
+    mainImage {
+      childImageSharp {
+        gatsbyImageData
       }
     }
+    Name
   }
 }
+
 `
